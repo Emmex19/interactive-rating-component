cb6a217fa3d9c5675863341246b557deb63f266e
Emmex19 <106507469+Emmex19@users.noreply.github.com>
2022-09-10 12:40:36 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,37 @@
+const spans = document.querySelectorAll('span');
+const form = document.querySelector('form');
+const ratingState = document.querySelector('#ratingstate');
+const afterSubmit = document.querySelector('.nodisplay');
+const secondHeader = document.querySelector('.appreciate');
+
+for (let span of spans) {
+    span.addEventListener('click', function () {
+        resetAllRatings();
+        this.style.backgroundColor = 'hsl(217, 12%, 63%)';
+        // const newDiv = document.createElement('div');
+        // newDiv.innerText = `You selected ${this.innerText} out of 5`;
+        // secondHeader.insertAdjacentElement('beforebegin', newDiv);
+        // newDiv.classList.add('ratingvalue');
+        let div = document.querySelector('.ratingvalue');
+        if (!div) {
+            div = document.createElement('div');
+            // div.innerText = `You selected ${this.innerText} out of 5`;
+            div.classList.add('ratingvalue');
+            secondHeader.insertAdjacentElement('beforebegin', div);
+        }
+        div.innerText = `You selected ${this.innerText} out of 5`;
+
+    })
+}
+
+function resetAllRatings() {
+    for (let span of spans) {
+        span.style.backgroundColor = ''
+    }
+}
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    ratingState.classList.toggle('nodisplay');
+    afterSubmit.classList.toggle('thankyoustate');
+})
